76a1514060aadb9418551d75bba928ac8801de21
osawa-koki <github@osawa.dev>
2023-02-21 02:44:27 +0900

    大枠完成した。

--- main.go
+++ main.go
@@ -16,12 +16,13 @@ func main() {
 	}
 
 	// 環境変数からエンドポイントの値を取得する
+	scheme := os.Getenv("SCHEME")
 	endpoint := os.Getenv("ENDPOINT")
 
 	engine:= gin.Default()
 
-	engine.GET("/:num", func(c *gin.Context) {
-		num_str := c.Param("num")
+	engine.GET("/:n", func(c *gin.Context) {
+		num_str := c.Param("n")
 		num, err := strconv.Atoi(num_str)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid number."})
@@ -38,8 +39,31 @@ func main() {
 			return
 		}
 
+    // HTTP GETリクエストを送信する
+    resp, err := http.Get(fmt.Sprintf("%s://%s/%d", scheme, endpoint, num))
+    if err != nil {
+      log.Fatal(err)
+    }
+    defer resp.Body.Close()
+
+    // レスポンスJSONをデコードする
+    var data map[string]interface{}
+    err = json.NewDecoder(resp.Body).Decode(&data)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    // "n"プロパティの値を整数として取得する
+    n, ok := data["n"].(float64)
+    if ok == false {
+      log.Fatal(`property "n" is not a number.`)
+    }
+
+    // nを整数として使用する
+    num := int(n)
+
 		// idを整数として使用する
-		c.JSON(http.StatusOK, gin.H{"n": n})
+		c.JSON(http.StatusOK, gin.H{"n": n * num, "error": nil})
 	})
 	engine.Run("0.0.0.0:8080")
 }
