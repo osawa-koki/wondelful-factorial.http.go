4d43f8d70b7e7ff348aa7d8624570c0f5bffcda3
osawa-koki <github@osawa.dev>
2023-02-21 02:40:17 +0900

    基礎部分。

--- main.go
+++ main.go
@@ -1,15 +1,45 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
-import "net/http"
+import (
+	"net/http"
+	"github.com/joho/godotenv"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+	// .envファイルから環境変数を読み込む
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+		return
+	}
+
+	// 環境変数からエンドポイントの値を取得する
+	endpoint := os.Getenv("ENDPOINT")
+
 	engine:= gin.Default()
-	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
+
+	engine.GET("/:num", func(c *gin.Context) {
+		num_str := c.Param("num")
+		num, err := strconv.Atoi(num_str)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid number."})
+			return
+		}
+
+		if (num < 1) {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Negative number."})
+			return
+		}
+
+		if (num == 1) {
+			c.JSON(http.StatusBadRequest, gin.H{"n": 1, "error": nil})
+			return
+		}
+
+		// idを整数として使用する
+		c.JSON(http.StatusOK, gin.H{"n": n})
 	})
 	engine.Run("0.0.0.0:8080")
 }
